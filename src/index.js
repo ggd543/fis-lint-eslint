4e7ef5158f315585eb0500c7f92495aa1bff56a4
刘永健 <yongjian.lyj@alibaba-inc.com>
2016-06-09 22:21:37 +0800

    feat: 支持deku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('colors');
 const eslint = require('eslint');
+const CLIEngine = eslint.CLIEngine;
 
 module.exports = (content, file, conf) => {
   let ignored = [];
@@ -18,24 +19,16 @@ module.exports = (content, file, conf) => {
       }
     }
   }
-  const linter = eslint.linter;
-  const ret = linter.verify(content, conf);
-  const outputs = [];
-  outputs.columnWidths = new Array(4).fill(0);
-  ret.forEach(e => {
-    if (e.fatal || e.severity) {
-      const strs = [`${e.line}:${e.column}`, (e.severity === 1 ? 'warning'.yellow : 'error'.red),
-               e.message, (e.ruleId || '').gray];
-      outputs.columnWidths = strs.map((s, j) => Math.max(outputs.columnWidths[j], s.length));
-      outputs.push(strs);
-    }
-  });
-  if (outputs.length > 0) {
-    console.log('\n', file.subpath);
-    outputs.forEach(line => {
-      const strs = line.map((s, j) => s + ' '.repeat(outputs.columnWidths[j] - s.length));
-      const s = strs.reduce((prevVal, curVal) => `${prevVal}${curVal}  `, ' '.repeat(2));
-      console.log(s);
-    });
+
+  const globals = Object.keys(conf.globals || {}).map(k => `${k}:${conf.globals[k]}`);
+  conf.globals = globals;
+
+  const cli = new CLIEngine(conf);
+
+  const report = cli.executeOnText(content, file.realpath);
+  const formatter = cli.getFormatter();
+  if (report.errorCount > 0 || report.warningCount > 0) {
+    console.log(formatter(report.results));
   }
+
 };
